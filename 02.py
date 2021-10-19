4551b37a46d6b6231a99774c1c60f133b75751db
Marina <90717933+jannybeam@users.noreply.github.com>
2021-10-19 20:44:09 +0300

    Add files via upload

--- 02.py
+++ 02.py
@@ -1,3 +1,43 @@
-num_list = [14, 122, 47, 28, 235, 2, 11, 91, 18]
-result = [num_list[i] for i in range(1, len(num_list)) if num_list[i] > num_list[i - 1]]
-print(result)
+from abc import ABC, abstractmethod
+
+class Clothes(ABC):
+    @abstractmethod
+    def consumption(self):
+        pass
+
+class Coat(Clothes):
+    def __init__(self, v):
+        self.v = v
+
+    @property
+    def consumption(self):
+        return self.v / 6.5 + 0.5
+
+    def sum_consumption(self, all_coats):
+        a = 0
+        for coat in all_coats:
+            a += coat.consumption
+        return a
+
+class Suit(Clothes):
+    def __init__(self, h):
+        self.h = h
+
+    @property
+    def consumption(self):
+        return 2 * self.h + 0.3
+
+    def sum_consumption(self, all_suits):
+        b = 0
+        for suit in all_suits:
+            b += suit.consumption
+        return b
+
+coat = Coat(42)
+suit = Suit(1.58)
+suit_2 = Suit(1.71)
+suit_3 = Suit(1.80)
+list_suits = [suit, suit_2, suit_3]
+print(coat.consumption)
+print(suit.consumption)
+print(suit.sum_consumption(list_suits))
