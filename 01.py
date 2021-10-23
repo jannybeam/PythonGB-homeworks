6e7fee6036f87257e189038342a03969deefd547
Marina <90717933+jannybeam@users.noreply.github.com>
2021-10-23 10:59:59 +0300

    Add files via upload

--- 01.py
+++ 01.py
@@ -1,10 +1,27 @@
-import sys
+class Date:
 
-if len(sys.argv) < 5:
-    print(f'Enter paramrters (output, rate, premium)')
-else:
-    a = float(sys.argv[1])
-    b = float(sys.argv[2])
-    c = float(sys.argv[3])
-    result = a * b * c
-    print(result)
+    def __init__(self, date):
+        self.date = date
+
+    @classmethod
+    def date_str_to_int(cls, date):
+        day, month, year = [int(i) for i in date.split('.')]
+        return f'{day}\n{month}\n{year}'
+
+    @staticmethod
+    def test_date(date):
+        day, month, year = [int(i) for i in date.split('.')]
+        if 0 < month < 13:
+            if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+                if 0 < day < 32:
+                    return True
+            elif month == 2:
+                if 0 < day < 29:
+                    return True
+            else:
+                if 0 < day < 31:
+                    return True
+        return False
+
+print(Date.date_str_to_int('21.10.2021'))
+print(Date.test_date('30.04.2021'))
